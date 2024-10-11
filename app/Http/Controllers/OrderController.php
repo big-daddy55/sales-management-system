99df7b0c149659b6910a76e01e0a8a80ec714b73
big-daddy <derrickazaglo123@gmail.com>
2024-10-11 07:58:24 +0000

    Update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function index(){
+        $orders = Order::all();
+
+        return response([
+            'message' => 'Success',
+            'data' => $orders
+        ]);
+    }
+    public function show($id){
+        $order = Order::findOrFail($id);
+
+        return response([
+            'message' => 'success',
+            'data' => $order
+        ]);
+    }
+    public function store(Request $request){
+        Order::create([
+            'product_id' => $request->product_id,
+            'quantity' => $request->quantity,
+            'total_amount' => $request->total_amount,
+            'price' => $request->price,
+        ]);
+    }
+    public function update(Request $request, $id){
+        $order = Order::findOrFail($id);
+
+        $order->update($request->all());
+        // return response()->json([
+        //     'message' => 'Order updated',
+        //     'data' => $order
+        // ]);
+
+        return response([
+            'message' => 'Order updated',
+            'data' => $order
+        ]);
+    }
+    public function delete($id){
+        $order = Order::findOrFail($id);
+
+        $order->delete($id);
+
+        return response([
+            'message' => 'Order deleted'
+        ]);
+    }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index(){
+    $products = Product::all();
+
+    return response([
+        'message' => 'success',
+        'data' => $products,
+    ]);
+    }
+
+    public function store(Request $request, ){
+        Product::create([
+            'name' => $request->name,
+            'quantity' => $request->quantity,
+            'price' => $request->price,
+        ]);
+
+        return response([
+            'message' => 'Product created',
+            'data' => $request->all()
+        ]);
+    }
+
+    public function show($id){
+        $product = Product::findOrFail($id);
+
+        return response([
+            'message' => 'Success',
+            'data' => $product,
+        ], 200);
+    }
+
+    public function update(Request $request, $id){
+        $product = Product::findOrFail($id);
+
+        $product->update($request->all());
+
+        $product->refresh();
+
+        return response([
+            'message' => 'Product updated',
+            'data' => $product,
+        ]);
+
+    }
+    public function delete($id){
+        $product = Product::findOrFail($id);
+
+        $product->delete($id);
+
+        return response([
+            'message' => 'Product deleted',
+        ]);
+    }
 }
